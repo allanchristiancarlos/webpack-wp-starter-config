8c1067a8f6ba2b8f69b3ef576d6d2d2a0337730a
Allan Christian Carlos <Allan.Carlos@nowcom.com.ph>
2017-04-26 08:03:05 +0800

    modify config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
-var path = require('path');
-
-var themeDir = path.resolve(__dirname, 'wp-content/themes/twentyseventeen');
+const path = require('path');
+const themeDir = path.resolve(__dirname, 'wp-content/themes/twentyseventeen');
+const assetsDir = themeDir + '/assets/';
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: {
-        app: themeDir + '/assets/js/app.js'
+        app: assetsDir + 'js/app.js'
     },
 
     output: {
@@ -15,9 +16,20 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ExtractTextPlugin.extract({
+                    fallback: '',
+                    use: 'css-loader'
+                })
+            },
+            {
                 test: /\.less$/,
                 exclude: /node_modules/,
-                loaders: 'style-loader!css-loader!less-loader'
+                use: ExtractTextPlugin.extract({
+                    fallback: '',
+                    use: 'css-loader!less-loader'
+                })
             },
             {
                 test: /\.js$/,
@@ -31,6 +43,13 @@ module.exports = {
             }
         ]
     },
+
+    plugins: [
+        new ExtractTextPlugin({
+            filename: 'css/[name].css',
+            disable: process.env.NODE_ENV == "development"
+        })
+    ],
 
     resolve: {
         extensions: ['.js']
